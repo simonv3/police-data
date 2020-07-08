39a81dd80f3dbf883c06e27eec6f1fb2b6c631c1
Simon <contact@vansintjan.net>
2020-07-08 10:53:04 -0700

    Updates

---
+++ precache-manifest.d47f5fe8deac9e195901c607887dffb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74a1671461eef3de6f97ceb4e42b256",
+    "url": "/police-data/index.html"
+  },
+  {
+    "revision": "e4de7c34b4fe40f437e8",
+    "url": "/police-data/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab3b5dd9ca65037d3c6d",
+    "url": "/police-data/static/js/2.d9a4efa3.chunk.js"
+  },
+  {
+    "revision": "cd2944d791bd3f36d76c7539fd5e4408",
+    "url": "/police-data/static/js/2.d9a4efa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4de7c34b4fe40f437e8",
+    "url": "/police-data/static/js/main.8769bafd.chunk.js"
+  },
+  {
+    "revision": "50b6f8885a24f901b983",
+    "url": "/police-data/static/js/runtime-main.3750c697.js"
+  }
+]);
